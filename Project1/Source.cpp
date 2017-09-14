1ea5171d12275150ce2989b28e0745d1dbf761d2
nathan <spyronath@gmail.com>
2017-09-14 18:50:22 -0300

    RFEHSFD

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,13 +2,27 @@
 
 using namespace std;
 
-int mano= 8; 
+int mano=0; 
 
 
 int main()
 {
 	cout << "Primeiraaaoo" << endl;
 	cout << 2 + 4 << endl;
-	system("pause");
-	return 0;
+	cout << "mande um numero entre 1 e 2" << endl; 
+	cin >> mano; 
+
+
+	if (mano == 1)
+	{
+		cout << " kkkk mano eu heim"<<endl;
+		system("pause");
+		return 0;
+	}
+	if (mano == 2)
+	{
+		cout << 2 + 4 << endl; 
+		system("pause");
+		return 0;
+	}
 }
